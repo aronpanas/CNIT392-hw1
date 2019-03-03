f26019cbb1dc130985cb44e9831b02d9964e549c
aronpanas <aronpanas@yahoo.com>
2019-03-03 18:25:06 -0500

    Update Artist.java

--- Artist.java
+++ Artist.java
@@ -1,19 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hw1;
 
+import java.io.Serializable;
+import javax.persistence.*;
+
 /**
- *
  * @author Aron
  */
-public class Artist 
+@Entity
+public class Artist implements Serializable
 {
-    private String currentuser;
-    private String currentmessage;
+    @Id @GeneratedValue
+       
+    private String artistName;
+    private String birthdate;
+    private String URL;
     
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
     
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", this.artistName, this.birthdate, this.URL);
+    }
     
 }
